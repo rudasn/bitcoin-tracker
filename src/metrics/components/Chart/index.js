af316e0aabb044baf64f5ee28d4b649df26bdfa3
Nicolas R <rudasn@gmail.com>
2018-08-22 07:10:34 +0300

    chart cleanup

--- src/metrics/components/Chart/index.js
+++ src/metrics/components/Chart/index.js
@@ -1,13 +1,3 @@
-import React, { PureComponent } from 'react'
-import { CanvasJSChart } from 'libs/canvasjs-2.2/canvasjs.react'
-
-class Chart extends PureComponent {
-  render() {
-    const { options } = this.props
-    return (
-      <CanvasJSChart options={ options }/>
-    )
-  }
-}
+import Chart from './Chart'
 
 export default Chart
