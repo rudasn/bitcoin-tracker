c0629225015c97cf2b44e68d605fa77c70312fba
Nicolas R <rudasn@gmail.com>
2018-08-22 12:51:50 +0300

    charts comments

--- src/metrics/components/Chart/Chart.js
+++ src/metrics/components/Chart/Chart.js
@@ -1,6 +1,11 @@
 import React, { PureComponent } from 'react'
 import { CanvasJSChart } from 'libs/canvasjs-2.2/canvasjs.react'
 
+/**
+ * A wrapper component around the CanvasJS library.
+ * In case we want to switch to a different charting library
+ * we only need to update this file.
+ */
 class Chart extends PureComponent {
   render() {
     const { options } = this.props
--- src/metrics/components/Chart/ChartTest.js
+++ src/metrics/components/Chart/ChartTest.js
@@ -2,6 +2,10 @@ import React, { PureComponent } from 'react'
 
 /**
  * A dummy chart component to be used for unit testing.
+ * We assume that the third-party Chart library has been
+ * independently unit tested and has passed all tests so
+ * we only want to test that our Chart component exports
+ * correctly and can be used in other components.
  */
 class TestChart extends PureComponent {
   render() {
