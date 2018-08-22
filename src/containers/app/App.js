ddeb0f3fbc9c4f69a63bfc5881d1e6ca76027384
Nicolas R <rudasn@gmail.com>
2018-08-22 16:36:14 +0300

    app style changes

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -7,18 +7,27 @@ class App extends PureComponent {
   render() {
     return (
       <div className="app">
-        <Bitfinex
-          currency="£"
-          property="last_price"
-          ticker="btcgbp"
-          title="BTC to GBP"
-        />
-        <Bitfinex
-          currency="£"
-          property="bid"
-          ticker="btcgbp"
-          title="BTC to GBP (bid)"
-        />
+        <h1 className="app-title">Outlyer Bitcoin Tracker</h1>
+        <main className="app-main">
+          <Bitfinex
+            currency="£"
+            property="last_price"
+            ticker="btcgbp"
+            title="BTC to GBP (Last Price)"
+          />
+          <Bitfinex
+            currency="£"
+            property="bid"
+            ticker="btcgbp"
+            title="BTC to GBP (Bid Price)"
+          />
+          <Bitfinex
+            currency=""
+            property="volume"
+            ticker="btcgbp"
+            title="BTC to GBP (Volume)"
+          />
+        </main>
         <footer>
           Copyright &copy;
         </footer>
